2f09168b5d25e1e9c25653f22f251392a30c0ccc
ipon1105 <my_world1105@mail.ru>
2021-05-12 17:28:12 +0300

    add new Info

--- sfml_project/MyGame.cpp
+++ sfml_project/MyGame.cpp
@@ -1,9 +1,41 @@
 #include <SFML/Graphics.hpp>
 
 namespace myGame {
+	using namespace sf;
 	
-	
-	void play() {
+	class Tile {
+	private:
+		FloatRect	position;//x,y,width,heitgh
+		Sprite		sprite;
+	public:
+		Tile(FloatRect rect) {
+			position = rect;
+		}
 
+		void initSprite(Texture &text, IntRect rect) {
+			sprite.setTexture(text);
+			sprite.setTextureRect(rect);
+		}
+
+		void render(RenderWindow window) {
+			sprite.setPosition(position.left, position.top);
+			window.draw(sprite);
+		}
+	};
+
+	void play() {
+		RenderWindow window(VideoMode(500, 500), "Game");
+
+		while (window.isOpen()) {
+
+			Event event;
+			while (window.pollEvent(event)) {
+				if (event.type == Event::Closed) window.close();
+			}
+
+			window.clear(Color::White);
+
+			window.display();
+		}
 	}
 }
